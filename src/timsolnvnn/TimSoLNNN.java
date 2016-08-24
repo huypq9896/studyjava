4df93665520a74d07a8730639c55a513a13b4e15
huyxh9896 <huypq9896@gmail.com>
2016-08-24 21:49:35 +0700

    create project

--- src/timsolnvnn/TimSoLNNN.java
+++ src/timsolnvnn/TimSoLNNN.java
@@ -5,10 +5,24 @@
  */
 package timsolnvnn;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Silk
  */
 public class TimSoLNNN {
-    
+    public static void main(String[] args) {
+        float a, b, c, max;
+        Scanner n = new Scanner (System.in);
+        a = n.nextFloat();
+        b = n.nextFloat();
+        c = n.nextFloat();
+        max = a;
+            if(max < b)
+                max = b;
+            if (max < c)
+                max = c;
+        System.out.println("So lon nhat = " +max);
+    }
 }
--- src/tongdayso/TongDaySo.java
+++ src/tongdayso/TongDaySo.java
@@ -5,10 +5,21 @@
  */
 package tongdayso;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Silk
  */
 public class TongDaySo {
-    
+    public static void main(String[] args) {
+        int i, n;
+        float E=1;
+        Scanner h= new Scanner (System.in);
+        n= h.nextInt();
+        System.out.println(" Nhap n: " +n);
+            for(i=1; i<=n; i++)
+                E= E + (float)1/i;
+                System.out.println(" Tong E la: " +E);
+    }
 }
